263ffc0bbf8261250f52bc4b882c7d92bf22b500
Ashman Malik <ashman@basiq.io>
2024-08-06 13:41:20 +1000

    DX-772 AV accounts validation

---
+++ utils/validation.js
@@ -0,0 +1,16 @@
+export const userIdRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
+export const limitRegex = /^[0-9]+$/;
+export const snapshotIdRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
+export const accountIdRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
+export const consentIdRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
+export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+
+export const validateUserId = (userId) => {
+  return userIdRegex.test(userId);
+};
+
+export const validateEmail = (email) => {
+    return emailRegex.test(email);
+};
+    
+  
