aa9fb9e4e92367c2d47290e29dcf9bb89a85ac4d
Ashman Malik <ashman@basiq.io>
2024-08-06 13:42:19 +1000

    DX-772 Updating Validation file

--- utils/validation.js
+++ utils/validation.js
@@ -1,8 +1,4 @@
 export const userIdRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
-export const limitRegex = /^[0-9]+$/;
-export const snapshotIdRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
-export const accountIdRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
-export const consentIdRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
 export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
 
 export const validateUserId = (userId) => {
