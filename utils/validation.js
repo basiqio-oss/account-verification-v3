f4f7e1f74e7315397b331124c4fffb240f88d755
Ashman Malik <ashman@basiq.io>
2025-02-27 14:34:51 +1000

    DX-906 Improve Email validation

--- utils/validation.js
+++ utils/validation.js
@@ -1,5 +1,5 @@
 export const userIdRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
-export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 export const validateUserId = (userId) => {
   return userIdRegex.test(userId);
