cb80b2d3d89a56910550ef02f8c60a5425def604
chris-basiq <basiqchris@outlook.com>
2025-02-27 14:06:07 +1100

    DX-983 Improve Email Validation Regex

--- utils/validation.js
+++ utils/validation.js
@@ -1,5 +1,5 @@
 export const userIdRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
-export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+export const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; 
 
 export const validateUserId = (userId) => {
   return userIdRegex.test(userId);
