ea3e05034be8ab319c6f2a3f009ad6556ace43b2
Ashman Malik <ashman@basiq.io>
2026-05-08 14:34:16 +1000

    DX-1008 Fix CI lint and Cypress configuration regressions

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    supportFile: 'cypress/integration/app.spec.js',
+    supportFile: 'cypress/support/index.js',
     specPattern: 'cypress/integration/**/*.js',
     setupNodeEvents(on, config) {
       // Example: require plugins if necessary
